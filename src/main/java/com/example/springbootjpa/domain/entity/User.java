02a296a9b1aa6e8e35065800b810d551cfc199f1
SuInWoo <wbo1026@naver.com>
2022-11-17 13:37:19 +0900

    springboot-jpa - User findById 추가

---
+++ src/main/java/com/example/springbootjpa/domain/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.springbootjpa.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String username;
+    String password;
+}
+
