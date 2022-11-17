5f1fe4162e438026fd3cf80e27586e7e42f6bb6d
SuInWoo <wbo1026@naver.com>
2022-11-17 14:20:34 +0900

    springboot-jpa - User add 추가

--- src/main/java/com/example/springbootjpa/domain/dto/UserRequest.java
+++ src/main/java/com/example/springbootjpa/domain/dto/UserRequest.java
@@ -1,5 +1,6 @@
 package com.example.springbootjpa.domain.dto;
 
+import com.example.springbootjpa.domain.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,4 +13,12 @@ import lombok.NoArgsConstructor;
 public class UserRequest {
     private String username;
     private String password;
+
+    public User toEntity() { // -> Article에 @Builder 추가
+        User user = User.builder()
+                .username(this.username)
+                .password(this.password) // title, content 두 개만 넣고도 article 구성 가능 -> @Builder로 인해
+                .build();
+        return user;
+    }
 }
--- src/main/java/com/example/springbootjpa/service/UserService.java
+++ src/main/java/com/example/springbootjpa/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.springbootjpa.service;
 
+import com.example.springbootjpa.domain.dto.UserRequest;
 import com.example.springbootjpa.domain.dto.UserResponse;
 import com.example.springbootjpa.domain.entity.User;
 import com.example.springbootjpa.repository.UserRepository;
@@ -24,5 +25,11 @@ public class UserService {
             User user = optUser.get();
             return new UserResponse(user.getId(), user.getUsername(), "");
         }
+    }
+
+    public UserResponse add(UserRequest dto){
+        User user = dto.toEntity(); // Entity로 변환
+        User savedUser = userRepository.save(user);
+        return new UserResponse(savedUser.getId(), savedUser.getUsername(), savedUser.getPassword());
     }
 }
